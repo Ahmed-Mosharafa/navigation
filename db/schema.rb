01786473e0bd17ee78ff9c005c985c01ea4da28c
Ahmed Mosharafa <ahmedmosharafa@gmail.com>
2015-10-04 23:54:55 -0300

    nearby places working

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20151003153101) do
+ActiveRecord::Schema.define(:version => 20151004150646) do
 
   create_table "places", :force => true do |t|
     t.float    "latitude"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(:version => 20151003153101) do
     t.integer  "range"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "name"
   end
 
 end
