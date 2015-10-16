c833ab2412e282b17356d772cd3eed0cb9fbaa4c
Ahmed-Mosharafa <ahmedmosharafa@gmail.com>
2015-10-16 16:29:39 +0200

    Delete 20151016130456_modify_beacons.rb

--- db/migrate/20151016130456_modify_beacons.rb
+++
@@ -1,7 +0,0 @@
-class Modify < ActiveRecord::Migration
-  def change
-    change_column :beacons, :coord_x, :float
-    change_column :beacons, :coord_y, :float
-    change_column :beacons, :coord_z, :float
-  end
-end
