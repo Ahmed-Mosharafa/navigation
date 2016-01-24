3504b36978221e4b2a8afe8be6d0db12c1da4b88
Ahmed Mosharafa <ahmedmosharafa@gmail.com>
2016-01-23 23:51:15 -0400

    absolute value was missing

--- app/models/finger_print.rb
+++ app/models/finger_print.rb
@@ -11,65 +11,65 @@ class FingerPrint < ActiveRecord::Base
   #if it doesn't exist it returns 0, otherwise it returns it's id
   def self.new_fingerprint(x,y, mac)
     same_fingerp = FingerPrint.where(:xcoord => x, :ycoord => y, :mac => mac).all
-  	#checks whether the record is empty or not
-  	if (FingerPrint.fetch_last_id !=0)
-  		#checks whether there are records with the same fingerprint or not
-  		last    = WifiFingerPrintsRecord.last
-	  	if (same_fingerp == [])
-	  		#pass the records to the calc mean_sd
-	  		#records is an array of fingerprints in WifiFingerPrintRecord with the same x,y,mac
-	  		records = WifiFingerPrintsRecord.where(:fingerprint_id => last[:fingerprint_id], :mac => last[:mac]).all 
-	  		FingerPrint.calculate_mean_sd(records, last[:fingerprint_id])
-	  		return 0
-	  	else
-	  		if (last[:mac] != mac)
-	  			FingerPrint.calculate_mean_sd(WifiFingerPrintsRecord.where(:fingerprint_id => last[:fingerprint_id], :mac => last[:mac]).all, last[:fingerprint_id])
-  			end
- 	  		return same_fingerp[0].id
-	  	end
-	else
-		return 0 
-	end 
+    #checks whether the record is empty or not
+    if (FingerPrint.fetch_last_id !=0)
+      #checks whether there are records with the same fingerprint or not
+      last    = WifiFingerPrintsRecord.last
+      if (same_fingerp == [])
+        #pass the records to the calc mean_sd
+        #records is an array of fingerprints in WifiFingerPrintRecord with the same x,y,mac
+        records = WifiFingerPrintsRecord.where(:fingerprint_id => last[:fingerprint_id], :mac => last[:mac]).all 
+        FingerPrint.calculate_mean_sd(records, last[:fingerprint_id])
+        return 0
+      else
+        if (last[:mac] != mac)
+          FingerPrint.calculate_mean_sd(WifiFingerPrintsRecord.where(:fingerprint_id => last[:fingerprint_id], :mac => last[:mac]).all, last[:fingerprint_id])
+        end
+        return same_fingerp[0].id
+      end
+  else
+    return 0 
+  end 
   end
   #calculates the standard deviation of the records with the same coordinates(of the same place)
   def self.calc_standard_deviation(mean, records)
-  	#debugger
-  	accum  = 0.0
-  	len = records.length
-  	records.each do |element|		
-  		accum = accum + ((element[:RSSI] - mean) ** 2)
-  	end
-  	sd = (accum/len) ** 0.5
-  	return sd
+    #debugger
+    accum  = 0.0
+    len = records.length
+    records.each do |element|   
+      accum = accum + ((element[:RSSI] - mean) ** 2)
+    end
+    sd = (accum/len) ** 0.5
+    return sd
   end
   #calculates the mean of the records with the same coordinates(of the same place)
   def self.calc_mean(records)
-  	accum   = 0.0
-  	len  = records.length
-  	records.each do |element|
-  		accum = accum + element[:RSSI] 
-  	end
-  	mean = accum / len
-  	return mean
+    accum   = 0.0
+    len  = records.length
+    records.each do |element|
+      accum = accum + element[:RSSI] 
+    end
+    mean = accum / len
+    return mean
   end
   #calls mean and standard deviation to save the mean RSSI and the SD to the record if you finished reading the fingerprints for this coordinates
   #with the same mac address(for the same router)
   #params: fingerprint_records: an array of the records with the same xcoord, ycoord, mac
   def self.calculate_mean_sd(fingerprint_records, last_id)
-	mean_RSSI = FingerPrint.calc_mean(fingerprint_records)
-  	sd_rssi   = FingerPrint.calc_standard_deviation(mean_RSSI, fingerprint_records)
-  	last = FingerPrint.find_by_id(last_id)
+  mean_RSSI = FingerPrint.calc_mean(fingerprint_records)
+    sd_rssi   = FingerPrint.calc_standard_deviation(mean_RSSI, fingerprint_records)
+    last = FingerPrint.find_by_id(last_id)
   ##
-	last.update_attributes(:RSSI => mean_RSSI, :SD => sd_rssi)
+  last.update_attributes(:RSSI => mean_RSSI, :SD => sd_rssi)
   end
 
   def self.fetch_last_id()
-  	fp = FingerPrint.last
-  	if (fp == nil)
-  		return 0
-  	else
-	  	id = fp[:id]
-	  end
+    fp = FingerPrint.last
+    if (fp == nil)
+      return 0
+    else
+      id = fp[:id]
+    end
   end
   
   #################################################
@@ -156,7 +156,7 @@ class FingerPrint < ActiveRecord::Base
       distances = Hash.new(k) 
       #find an array of distances 
       records.each  do |record|
-        distance = record.RSSI - rssi_searched
+        distance = abs(record.RSSI - rssi_searched)
         distances = FingerPrint.add_to_hash([record.xcoord, record.ycoord], distance, distances, k)
       end
       #here I have the k coordinates of the nearest neighbours along with their distances
@@ -176,4 +176,4 @@ class FingerPrint < ActiveRecord::Base
   end
 end
 
-		
+    
