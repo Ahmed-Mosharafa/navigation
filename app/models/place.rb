76f681d79f9917bd0499e04dad1c1a9805580733
Ahmed Mosharafa <ahmedmosharafa@gmail.com>
2016-01-28 21:36:11 -0400

    removing maplink

--- app/models/place.rb
+++ app/models/place.rb
@@ -3,13 +3,6 @@ class Place < ActiveRecord::Base
   has_many :beacons
   has_many :routers
   has_many :fingerprints
-  has_attached_file :map, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
-  validates_attachment_content_type :map, content_type: /\Aimage\/.*\Z/
-  acts_as_mappable :default_units => :miles,
-                   :default_formula => :sphere,
-                   :distance_field_name => :distance,
-                   :lat_column_name => :latitude,
-                   :lng_column_name => :longitude
   def self.find_nearby(range, lat, long)
     return Place.within(range,:origin => [lat,long]) 
   end
