997e216d5d81a48df65f5f39d22df48ce8b9f3c5
Ahmed Mosharafa <ahmedmosharafa@gmail.com>
2016-01-31 00:45:56 -0400

    -

--- db/migrate/20160128221305_remove_foreign_keys.rb
+++ db/migrate/20160128221305_remove_foreign_keys.rb
@@ -1,4 +1,4 @@
-class AddForeignKeys < ActiveRecord::Migration
+class RemoveForeignKeys < ActiveRecord::Migration
   def change
     remove_foreign_key "beacons", "places", name: "beacons_place_id_fk", dependent: :delete
     remove_foreign_key "finger_prints", "places", name: "finger_prints_place_id_fk", dependent: :delete
