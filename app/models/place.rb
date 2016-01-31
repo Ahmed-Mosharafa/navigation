7e00055faa7542ee5b099839c8008f4659d09ade
Ahmed Mosharafa <ahmedmosharafa@gmail.com>
2016-01-31 18:41:26 -0400

    nearby

--- app/models/place.rb
+++ app/models/place.rb
@@ -3,6 +3,11 @@ class Place < ActiveRecord::Base
   has_many :beacons
   has_many :routers
   has_many :fingerprints
+  acts_as_mappable :default_units => :miles,
+                   :default_formula => :sphere,
+                   :distance_field_name => :distance,
+                   :lat_column_name => :latitude,
+                   :lng_column_name => :longitude
   def self.find_nearby(range, lat, long)
     return Place.within(range,:origin => [lat,long]) 
   end
