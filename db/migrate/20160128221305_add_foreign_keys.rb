49e6716888ef40636d62ebf0b22f2198dd117b4a
Ahmed Mosharafa <ahmedmosharafa@gmail.com>
2016-01-31 00:34:19 -0400

    -

--- db/migrate/20160128221305_add_foreign_keys.rb
+++
@@ -1,8 +0,0 @@
-class AddForeignKeys < ActiveRecord::Migration
-  def change
-    add_foreign_key "beacons", "places", name: "beacons_place_id_fk", dependent: :delete
-    add_foreign_key "finger_prints", "places", name: "finger_prints_place_id_fk", dependent: :delete
-    add_foreign_key "routers", "places", name: "routers_place_id_fk", dependent: :delete
-    add_foreign_key "wifi_finger_prints_records", "finger_prints" ,name: "wifi_finger_prints_records_fingerprint_id_fk", dependent: :delete
-  end
-end
